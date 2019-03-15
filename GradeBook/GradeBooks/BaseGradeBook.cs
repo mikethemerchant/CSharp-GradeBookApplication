10f412bb9f56ab9d940ca936b54924cbe787cb3e
Michael Bender <mikethemerchant@gmail.com>
2019-03-15 16:07:03 -0400

    final changes

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +109,30 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int returnValue = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    returnValue = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    returnValue = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    returnValue = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    returnValue = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    returnValue = 0;
+                    break;
             }
-            return 0;
+            if (this.IsWeighted == true && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled) )
+            {
+                returnValue++;
+            }
+            return returnValue;
         }
 
         public virtual void CalculateStatistics()
